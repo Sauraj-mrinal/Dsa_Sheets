34e22bcb05e5bf507695edd06f7751ac7dbc3c0a
Mrinal Sauraj <149461953+Sauraj-mrinal@users.noreply.github.com>
2023-12-09 23:16:57 +0530

    Create 189.rotate-array.java

---
+++ 189.rotate-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[] a, int k) {
+        int n=a.length;
+        k = k % a.length;
+        if (k < 0) {
+           k += a.length;
+       }
+       reverse(a, 0, n - k - 1);
+       reverse(a, n - k, n - 1);
+       reverse(a, 0, n- 1);
+   }
+    private static void reverse(int[] a, int i, int j) {
+
+       while (i < j) {
+           int temp = a[i];
+           a[i] = a[j];
+           a[j] = temp;
+           i++;
+           j--;
+       }
+    }
+}
